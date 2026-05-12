6f51ad0dae4e01ced5f10414f2f75eae7145f18d
omendesx <otavio.mendes090110@gmail.com>
2026-05-11 23:45:08 -0300

    refactor: Convert to REST API - return JSON instead of HTML

--- cadastro-alunos/src/main/java/br/com/escola/cadastro_alunos/controller/AlunoController.java
+++ cadastro-alunos/src/main/java/br/com/escola/cadastro_alunos/controller/AlunoController.java
@@ -1,21 +1,17 @@
-package br.com.escola.cadastro_alunos.controller;
+﻿package br.com.escola.cadastro_alunos.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.ui.Model;
 import br.com.escola.cadastro_alunos.repository.AlunoRepository;
 import br.com.escola.cadastro_alunos.model.Aluno;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
-import java.lang.ProcessBuilder.Redirect;
-import java.util.List;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class AlunoController {
 
     private final AlunoRepository alunoRepository;
@@ -24,43 +20,32 @@ public class AlunoController {
         this.alunoRepository = alunoRepository;
     }
 
-    @GetMapping
-    public String paginaInicial(){
-        return "redirect:/alunos";
+    @GetMapping("/alunos")
+    public List<Aluno> listarAlunos(){
+        return alunoRepository.listarTodos();
     }
 
-    @GetMapping("/alunos")
-public String listarAlunos(Model model){
-        model.addAttribute("aluno", new Aluno());
-        model.addAttribute("alunos", alunoRepository.listarTodos());
-        return "alunos";
-}
+    @PostMapping("/alunos")
+    public Aluno salvarAluno(@RequestBody Aluno aluno){
+        alunoRepository.salvar(aluno);
+        return aluno;
+    }
 
-@PostMapping("/alunos")
-public String salvarAluno(@ModelAttribute Aluno aluno, RedirectAttributes redirectAttributes){
-    alunoRepository.salvar(aluno);
-    redirectAttributes.addFlashAttribute("mensagem", "Aluno cadastrado com sucesso!");
-    return "redirect:/alunos";
-}
+    @GetMapping("/alunos/{id}")
+    public Aluno buscarAluno(@PathVariable Integer id){
+        return alunoRepository.buscarPorIdAluno(id);
+    }
 
-@GetMapping("/alunos/{id}/editar")
-public String abrirFormularEdicao(@PathVariable Integer id, Model model){
-    Aluno aluno = alunoRepository.buscarPorIdAluno(id);
-    model.addAttribute("aluno", aluno);
-    return "editar-aluno";
-}
-@PostMapping("/alunos/atualizar")
-public String atualizarAluno(@ModelAttribute Aluno aluno, RedirectAttributes redirectAttributes){
-    alunoRepository.atualizar(aluno);
-    redirectAttributes.addFlashAttribute("mensagem", "Aluno atualizado com sucesso!");
-    return "redirect:/alunos";
-}
+    @PostMapping("/alunos/{id}/atualizar")
+    public Aluno atualizarAluno(@PathVariable Integer id, @RequestBody Aluno aluno){
+        aluno.setId(id);
+        alunoRepository.atualizar(aluno);
+        return aluno;
+    }
 
-@PostMapping("/alunos/{id}/excluir")
-public String excluirAluno(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
-    alunoRepository.excluir(id);
-    redirectAttributes.addFlashAttribute("mensagem", "Aluno excluído com sucesso!");
-    return "redirect:/alunos";
-}
+    @DeleteMapping("/alunos/{id}")
+    public void excluirAluno(@PathVariable Integer id) {
+        alunoRepository.excluir(id);
+    }
 
 }
